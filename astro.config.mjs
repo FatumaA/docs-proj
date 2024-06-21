5a241b99069aa290be7376af693412a55247a836
FatumaA <67555014+FatumaA@users.noreply.github.com>
2024-06-21 16:45:56 +0300

    build: add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from "astro/config";
+import netlify from "@astrojs/netlify";
 import starlight from "@astrojs/starlight";
 import tailwind from "@astrojs/tailwind";
 
@@ -6,32 +7,45 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "hybrid",
-  integrations: [starlight({
-    title: "",
-    logo: {
-      src: "./src/assets/logo.png",
-      replacesTitle: true
-    },
-    social: {
-      github: "https://github.com/FatumaA/docs-proj"
-    },
-    customCss: [
-    // Path to your Tailwind base styles:
-    "./src/tailwind.css", "./src/assets/landing.css", "@fontsource/nunito-sans/400.css", "@fontsource/nunito-sans/600.css"],
-    sidebar: [{
-      label: "Overview",
-      link: "/docs"
-    }, {
-      label: "Guides",
-      autogenerate: {
-        directory: "docs/guides"
-      }
-    }, {
-      label: "Reference",
-      autogenerate: {
-        directory: "docs/reference"
-      }
-    }]
-  }), tailwind(), icon()]
+	output: "hybrid",
+	adapter: netlify(),
+	integrations: [
+		starlight({
+			title: "",
+			logo: {
+				src: "./src/assets/logo.png",
+				replacesTitle: true,
+			},
+			social: {
+				github: "https://github.com/FatumaA/docs-proj",
+			},
+			customCss: [
+				// Path to your Tailwind base styles:
+				"./src/tailwind.css",
+				"./src/assets/landing.css",
+				"@fontsource/nunito-sans/400.css",
+				"@fontsource/nunito-sans/600.css",
+			],
+			sidebar: [
+				{
+					label: "Overview",
+					link: "/docs",
+				},
+				{
+					label: "Guides",
+					autogenerate: {
+						directory: "docs/guides",
+					},
+				},
+				{
+					label: "Reference",
+					autogenerate: {
+						directory: "docs/reference",
+					},
+				},
+			],
+		}),
+		tailwind(),
+		icon(),
+	],
 });
