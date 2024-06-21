3f0715361badcd545f52f08f227114efe534b2f2
FatumaA <67555014+FatumaA@users.noreply.github.com>
2024-06-21 14:18:46 +0300

    style: customise base colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,29 +1,21 @@
 import starlightPlugin from "@astrojs/starlight-tailwind";
-
-// Generated color palettes
-const accent = {
-	200: "#a6d5be",
-	600: "#007f5a",
-	900: "#003d29",
-	950: "#002c1d",
-};
-const gray = {
-	100: "#f6f6f6",
-	200: "#eeeeee",
-	300: "#c2c2c2",
-	400: "#8b8b8b",
-	500: "#585858",
-	700: "#383838",
-	800: "#272727",
-	900: "#181818",
-};
+import colors from "tailwindcss/colors";
 
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		extend: {
-			colors: { accent, gray },
+			colors: {
+				// Your preferred accent color. Indigo is closest to Starlight’s defaults.
+				accent: colors.green,
+				// Your preferred gray scale. Zinc is closest to Starlight’s defaults.
+				gray: colors.zinc,
+			},
+			fontFamily: {
+				// Your preferred text font. Starlight uses a system font stack by default.
+				sans: ["Nunito Sans"],
+			},
 		},
 	},
 	plugins: [starlightPlugin()],
