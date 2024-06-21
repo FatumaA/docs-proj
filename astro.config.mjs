ea4d7e7ef21927f0bdf641bcc745e2bc614be59f
FatumaA <67555014+FatumaA@users.noreply.github.com>
2024-06-21 16:38:23 +0300

    build: add icons, adjust render mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,46 +1,37 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
-
 import tailwind from "@astrojs/tailwind";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: "",
-			logo: {
-				src: "./src/assets/logo.png",
-				replacesTitle: true,
-			},
-			social: {
-				github: "https://github.com/FatumaA/docs-proj",
-			},
-			customCss: [
-				// Path to your Tailwind base styles:
-				"./src/tailwind.css",
-				"./src/assets/landing.css",
-				"@fontsource/nunito-sans/400.css",
-				"@fontsource/nunito-sans/600.css",
-			],
-			sidebar: [
-				{
-					label: "Overview",
-					link: "/docs",
-				},
-				{
-					label: "Guides",
-					autogenerate: {
-						directory: "docs/guides",
-					},
-				},
-				{
-					label: "Reference",
-					autogenerate: {
-						directory: "docs/reference",
-					},
-				},
-			],
-		}),
-		tailwind(),
-	],
+  output: "hybrid",
+  integrations: [starlight({
+    title: "",
+    logo: {
+      src: "./src/assets/logo.png",
+      replacesTitle: true
+    },
+    social: {
+      github: "https://github.com/FatumaA/docs-proj"
+    },
+    customCss: [
+    // Path to your Tailwind base styles:
+    "./src/tailwind.css", "./src/assets/landing.css", "@fontsource/nunito-sans/400.css", "@fontsource/nunito-sans/600.css"],
+    sidebar: [{
+      label: "Overview",
+      link: "/docs"
+    }, {
+      label: "Guides",
+      autogenerate: {
+        directory: "docs/guides"
+      }
+    }, {
+      label: "Reference",
+      autogenerate: {
+        directory: "docs/reference"
+      }
+    }]
+  }), tailwind(), icon()]
 });
