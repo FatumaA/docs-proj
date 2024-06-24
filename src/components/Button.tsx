53cd6956227608a34b8e1c0a9eca33fb7cf4d3dd
FatumaA <67555014+FatumaA@users.noreply.github.com>
2024-06-24 14:21:42 +0300

    fix: replace astro component with a react component

---
+++ src/components/Button.tsx
@@ -0,0 +1,58 @@
+// "use client";
+import { type IconType } from "react-icons";
+import { MdOutlineRemoveRedEye } from "react-icons/md";
+
+export enum BtnActions {
+	ViewPhoto = "download",
+	SeeMore = "seeMore",
+}
+
+interface ButtonProps {
+	icon?: IconType;
+	size?: number;
+	buttonText: string;
+	iconClass?: string;
+	withIcon?: boolean;
+	onClickHandler?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+	btnAction: BtnActions;
+	actionURL: string;
+}
+
+const Button = ({
+	icon: IconComponent,
+	size,
+	buttonText,
+	iconClass,
+	withIcon = false,
+	onClickHandler,
+	btnAction,
+	actionURL,
+}: ButtonProps) => {
+	const clickHandler = () => {
+		console.log("Button clicked");
+		if (btnAction === BtnActions.ViewPhoto) {
+			window.open(actionURL);
+		} else {
+			console.log("FROM RICK MORTY");
+			window.location.href = actionURL;
+		}
+	};
+
+	return (
+		<button
+			id="btn"
+			className="bg-green-300 hover:bg-green-200 text-black font-bold px-4 py-2 rounded-md cursor-pointer flex ml-auto items-center gap-2"
+			onClick={clickHandler}
+		>
+			<span>{buttonText}</span>
+			{withIcon && (
+				<MdOutlineRemoveRedEye size={size ?? 24} className={iconClass} />
+			)}
+			{/* {withIcon && IconComponent && (
+				<IconComponent size={size ?? 24} className={iconClass} />
+			)} */}
+		</button>
+	);
+};
+
+export default Button;
