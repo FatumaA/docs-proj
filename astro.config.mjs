a9dc4aa6ce14aa16a5d14584f0297bdaca80c2ab
FatumaA <67555014+FatumaA@users.noreply.github.com>
2024-06-25 15:30:51 +0300

    fix: switch to server to fix astro.reqs issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,53 +6,65 @@ import icon from "astro-icon";
 import react from "@astrojs/react";
 import node from "@astrojs/node";
 
-
 // https://astro.build/config
 export default defineConfig({
-  image: {
-    remotePatterns: [{
-      protocol: "https"
-    }]
-  },
-  output: "hybrid",
-  adapter: netlify(),
-  integrations: [starlight({
-    title: "",
-    logo: {
-      src: "./src/assets/logo.png",
-      replacesTitle: true
-    },
-    social: {
-      github: "https://github.com/FatumaA/docs-proj"
-    },
-    customCss: [
-    // Path to your Tailwind base styles:
-    "./src/tailwind.css", "./src/assets/landing.css", "@fontsource/nunito-sans/400.css", "@fontsource/nunito-sans/600.css"],
-    sidebar: [{
-      label: "Overview",
-      link: "/docs"
-    }, {
-      label: "Guides",
-      autogenerate: {
-        directory: "docs/guides"
-      }
-    }
-    // {
-    // 	label: "Reference",
-    // 	autogenerate: {
-    // 		directory: "docs/reference",
-    // 	},
-    // },
-    ]
-  }), tailwind(), icon(), react({
-    experimentalReactChildren: true
-  })],
-  //   react({
-  //     experimentalReactChildren: true
-  //   })],
-  vite: {
-    ssr: {
-      noExternal: ["react-icons"]
-    }
-  }
+	image: {
+		remotePatterns: [
+			{
+				protocol: "https",
+			},
+		],
+	},
+	output: "server",
+	adapter: netlify(),
+	integrations: [
+		starlight({
+			title: "",
+			logo: {
+				src: "./src/assets/logo.png",
+				replacesTitle: true,
+			},
+			social: {
+				github: "https://github.com/FatumaA/docs-proj",
+			},
+			customCss: [
+				// Path to your Tailwind base styles:
+				"./src/tailwind.css",
+				"./src/assets/landing.css",
+				"@fontsource/nunito-sans/400.css",
+				"@fontsource/nunito-sans/600.css",
+			],
+			sidebar: [
+				{
+					label: "Overview",
+					link: "/docs",
+				},
+				{
+					label: "Guides",
+					autogenerate: {
+						directory: "docs/guides",
+					},
+				},
+				// {
+				// 	label: "Reference",
+				// 	autogenerate: {
+				// 		directory: "docs/reference",
+				// 	},
+				// },
+			],
+		}),
+		tailwind(),
+		icon(),
+		react({
+			experimentalReactChildren: true,
+		}),
+	],
+	//   react({
+	//     experimentalReactChildren: true
+	//   })],
+	vite: {
+		ssr: {
+			noExternal: ["react-icons"],
+		},
+	},
 });
