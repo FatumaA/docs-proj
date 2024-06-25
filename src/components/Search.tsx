4785b5c3a04a6b2a917cd9bba1669bb46a877a4a
FatumaA <67555014+FatumaA@users.noreply.github.com>
2024-06-25 15:31:29 +0300

    feat: add search API functionality

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,14 +1,20 @@
 import { useState } from "react";
 
 import Button from "../components/Button";
+import type { UnsplashPhoto } from "../models/unsplash";
 
-const Search = () => {
+interface SearchProps {
+	auth: string;
+}
+
+const Search = ({ auth }: SearchProps) => {
 	const [searchTerm, setSearchTerm] = useState("");
 	const [isSearching, setIsSearching] = useState(false);
 	const [error, setError] = useState("");
 
 	const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
 		event.preventDefault();
+
 		if (!searchTerm) {
 			setError("No search term entered");
 			setTimeout(() => {
@@ -19,18 +25,41 @@ const Search = () => {
 
 		setIsSearching(true);
 
-		// const res = await searchTasks(searchTerm);
-		// console.log("res search: ", res);
-		// if (res.length === 0) {
-		// 	setIsSearching(false);
-		// 	setError("No task found");
-		// 	setTimeout(() => {
-		// 		setSearchTerm("");
-		// 		setError("");
-		// 	}, 3000);
-		// 	return;
-		// }
-		setIsSearching(false);
+		try {
+			const res = await fetch(
+				`https://api.unsplash.com/search/photos?query=${searchTerm}`,
+				{
+					method: "GET",
+					headers: {
+						Authorization: auth,
+					},
+				}
+			);
+			// console.log("res search: ", res);
+			const photoRes = await res.json();
+			console.log(JSON.stringify(photoRes));
+			if (photoRes.results.length === 0) {
+				setIsSearching(false);
+				setError("No photos found");
+				setTimeout(() => {
+					setSearchTerm("");
+					setError("");
+				}, 3000);
+				return;
+			}
+			setIsSearching(false);
+
+			// Handle the photo results
+			window.localStorage.setItem(
+				"photoResults",
+				JSON.stringify(photoRes.results)
+			);
+			window.location.href = `/unsplash-exp/search-results`;
+		} catch (error) {
+			console.error("Error fetching data: ", error);
+			setError("An error occurred while searching" + error);
+			setIsSearching(false);
+		}
 	};
 	return (
 		<div className="flex flex-col w-full md:w-1/2">
@@ -40,6 +69,7 @@ const Search = () => {
 			>
 				<input
 					aria-roledescription="search"
+					name="search"
 					type="text"
 					id="search"
 					placeholder="search unsplash API for photos..."
